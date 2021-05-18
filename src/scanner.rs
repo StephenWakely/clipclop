22f80d8e8cdca8ae3a922e9fc126e80b3b78ac40
Stephen Wakely <fungus.humungus@gmail.com>
2021-05-18 10:33:32 +0100

    Retry on clipboard errors
    Signed-off-by: Stephen Wakely <fungus.humungus@gmail.com>

--- src/scanner.rs
+++ src/scanner.rs
@@ -2,7 +2,7 @@ use crate::client::{connect, send_clipboard};
 use copypasta::{ClipboardContext, ClipboardProvider};
 use tokio::time::{sleep, Duration};
 use tonic::transport::Uri;
-use tracing::{debug, info};
+use tracing::{debug, error, info};
 
 /// Scans the clipboard once per second. If it changes, we send it to the server.
 pub async fn clipboard(server: Uri) {
@@ -15,13 +15,17 @@ pub async fn clipboard(server: Uri) {
     info!("Scanning clipboard");
     loop {
         sleep(Duration::from_secs(1)).await;
-        let next = clipboard.get_contents().unwrap();
-        if next != contents {
-            contents = next;
-            debug!("**{}**", contents);
-            if !contents.is_empty() {
-                send_clipboard(&mut client, contents.clone()).await;
+        match clipboard.get_contents() {
+            Ok(next) => {
+                if next != contents {
+                    contents = next;
+                    debug!("**{}**", contents);
+                    if !contents.is_empty() {
+                        send_clipboard(&mut client, contents.clone()).await;
+                    }
+                }
             }
+            Err(err) => error!("Error reading clipboard {:?}", err),
         }
     }
 }
